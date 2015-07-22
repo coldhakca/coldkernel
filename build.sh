ab0533f531bc4812b33fa3fd2491c3c2807ddbad
Colin Childs <colin@torproject.org>
2015-07-21 23:36:05 -0500

    Added verbose flag, refactored

--- build.sh
+++ build.sh
@@ -13,70 +13,50 @@ KERNEL_VERSION=linux-4.0.8
 
 # Fetch Greg & Spender's keys
 function get_keys () {
-    start_spinner "Receiving Greg Kroah-Hartman's signing key..."
-    gpg -q --recv-key 647F28654894E3BD457199BE38DBBDC86092693E > /dev/null 2>&1 &&
-    stop_spinner $?
-    start_spinner "Receiving Spender's signing key..."
-    gpg -q --recv-key DE9452CE46F42094907F108B44D1C0F82525FE49 > /dev/null 2>&1 
-    stop_spinner $?
+    gpg --recv-key 647F28654894E3BD457199BE38DBBDC86092693E
+    gpg --recv-key DE9452CE46F42094907F108B44D1C0F82525FE49
 }
 
 # Fetch Linux Kernel sources and signatures
 function get_kernel () {
-    start_spinner "Fetching kernel sources and signatures..."
-    wget --no-clobber $KERNEL/$KERNEL_VERSION.tar.{sign,xz} > /dev/null 2>&1
-    stop_spinner $?
+    wget $KERNEL/$KERNEL_VERSION.tar.{sign,xz}
 }
 
 # Fetch Kernel patch sources and signatures
 function get_patches () {
-    start_spinner "Fetching grsecurity patch and signatures..."
-    wget --no-clobber $GRSECURITY/$GRSECURITY_VERSION.{patch.sig,patch} > /dev/null 2>&1
-    stop_spinner $?
+    wget $GRSECURITY/$GRSECURITY_VERSION.{patch.sig,patch}
 }
 
 # Unxz Kernel
 function unpack_kernel () {
-    start_spinner "Unpacking Linux Kernel sources..."
     unxz $KERNEL_VERSION.tar.xz
-    stop_spinner $?
 }
 
 # Verify Linux Kernel sources
 function verify_kernel () {
-    start_spinner "Verifying the Linux Kernel sources..."
-    gpg --verify $KERNEL_VERSION.{tar.sign,tar} > /dev/null 2>&1
-    stop_spinner $?
+    gpg --verify $KERNEL_VERSION.{tar.sign,tar}
 }
 
 # Verify Kernel patches
 function verify_patches () {
-    start_spinner "Verifying Kernel patches..."
-    gpg --verify $GRSECURITY_VERSION.{patch.sig,patch} > /dev/null 2>&1
-    stop_spinner $?
+    gpg --verify $GRSECURITY_VERSION.{patch.sig,patch}
 }
 
 # Extract Linux Kernel
 function extract_kernel () {
-    start_spinner "Extracting Linux Kernel sources..."
-    tar -xf $KERNEL_VERSION.tar
-    stop_spinner $?
+    tar -xvf $KERNEL_VERSION.tar
 }
 
 # Patch the kernel with grsec, and apply coldkernel config
 function patch_kernel () {
-    start_spinner "Applying grsecurity patch, and moving coldkernel.config into place..."
     cd $KERNEL_VERSION &&
-    patch -s -p1 < ../$GRSECURITY_VERSION.patch
-    cp ../coldkernel.config .config > /dev/null 2>&1
-    stop_spinner $?
+    patch -p1 < ../$GRSECURITY_VERSION.patch
+    cp ../coldkernel.config .config
 }
 
 # Build coldkernel on Debian
 function build_kernel () {
-    start_spinner "Building coldkernel..."
-    fakeroot make deb-pkg > /dev/null 2>&1
-    stop_spinner $?
+    fakeroot make deb-pkg
 }
 
 #	      /\
@@ -92,13 +72,46 @@ function build_kernel () {
 #  Do the coldkernel happy dance
 #  ------------------------------
 
-get_keys &&
-get_kernel &&
-get_patches &&
-unpack_kernel &&
-verify_kernel &&
-verify_patches &&
-extract_kernel &&
-patch_kernel &&
-build_kernel
-
+case "$1" in
+                -v)
+                get_keys &&
+                get_kernel &&
+                get_patches &&
+                unpack_kernel &&
+                verify_kernel &&
+                verify_patches &&
+                extract_kernel &&
+                patch_kernel &&
+                build_kernel
+                exit 0;;
+		
+		*)
+		start_spinner "Receiving signing keys..."
+		get_keys > /dev/null 2>&1 &&
+		stop_spinner $?
+		start_spinner "Fetching kernel sources and signatures..."
+		get_kernel > /dev/null 2>&1 &&
+		stop_spinner $?
+		start_spinner "Fetching grsecurity patch and signatures..."
+		get_patches > /dev/null 2>&1 &&
+		stop_spinner $?
+		start_spinner "Unpacking Linux Kernel sources..."
+		unpack_kernel > /dev/null 2>&1 &&
+		stop_spinner $?
+		start_spinner "Verifying the Linux Kernel sources..."
+		verify_kernel > /dev/null 2>&1 &&
+		stop_spinner $?
+		start_spinner "Verifying Kernel patches..."
+		verify_patches > /dev/null 2>&1 &&
+		stop_spinner $?
+		start_spinner "Extracting Linux Kernel sources..."
+		extract_kernel > /dev/null 2>&1 &&
+		stop_spinner $?
+		start_spinner "Applying grsecurity patch, and moving coldkernel.config into place..."
+		patch_kernel > /dev/null 2>&1 &&
+		stop_spinner $?
+		start_spinner "Building coldkernel..."
+		build_kernel > /dev/null 2>&1
+		stop_spinner $?
+		exit 0;;
+esac
