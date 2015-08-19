dba8559dd4f55b4fc5fda0913316181dde19085a
Colin Childs <colin@torproject.org>
2015-08-18 23:45:57 -0500

    Bumping versions to 4.1.6

--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 # ColdKernel build script
 # build 0.2a
-# 4.1.5-grsec-coldkernel
+# 4.1.6-grsec-coldkernel
 # ColdHak (C. // J. // R. // T.)
 
 source "$(pwd)/spinner.sh"
 
 GRSECURITY=https://grsecurity.net/test/
-GRSECURITY_VERSION="$(curl --silent https://grsecurity.net/testing_rss.php | sed -ne 's/.*\(http[^"]*\).patch/\1/p' | sed 's/<.*//' | sed 's/^.*grsecurity-3.1-4.1.5/grsecurity-3.1-4.1.5/' | sed -n '1p')"
+GRSECURITY_VERSION="$(curl --silent https://grsecurity.net/testing_rss.php | sed -ne 's/.*\(http[^"]*\).patch/\1/p' | sed 's/<.*//' | sed 's/^.*grsecurity-3.1-4.1.6/grsecurity-3.1-4.1.6/' | sed -n '1p')"
 KERNEL=https://www.kernel.org/pub/linux/kernel/v4.x
-KERNEL_VERSION=linux-4.1.5
+KERNEL_VERSION=linux-4.1.6
 
 # Fetch Greg & Spender's keys
 function import_keys () {
