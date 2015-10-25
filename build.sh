17bc7b2ef405f49c00c0f5b4b2765178c95fea5e
Colin Childs <colin@torproject.org>
2015-10-25 01:14:15 -0500

    Formatting changes

--- build.sh
+++ build.sh
@@ -19,12 +19,12 @@ function import_keys () {
 
 # Remove Linux working directory
 function remove_wrkdir () {
-if [ ! -d $KERNEL_VERSION ]
-then
-   echo "Directory doesn't exist"
-else
-    rm -r $KERNEL_VERSION
-fi
+    if [ ! -d $KERNEL_VERSION ]
+    then
+	echo "Directory doesn't exist"
+    else
+	rm -r $KERNEL_VERSION
+    fi
 }
 
 # Fetch Linux Kernel sources and signatures
@@ -34,14 +34,14 @@ function get_kernel () {
 
 # Fetch Kernel patch sources and signatures
 function get_patches () {
-if [ ! -d patches ]
-then
-    git clone https://github.com/coldhakca/deepfreeze patches
-else
-    cd patches
-    git pull
-    cd ..
-fi
+    if [ ! -d patches ]
+    then
+	git clone https://github.com/coldhakca/deepfreeze patches
+    else
+	cd patches
+	git pull
+	cd ..
+    fi
 }
 
 # Unxz Kernel
@@ -67,7 +67,7 @@ function extract_kernel () {
 # Patch the kernel with grsec, and apply coldkernel config
 function patch_kernel () {
     cd $KERNEL_VERSION &&
-    patch -p1 < ../patches/grsecurity/grsecurity-3.1-4.2.4-*.patch
+	patch -p1 < ../patches/grsecurity/grsecurity-3.1-4.2.4-*.patch
     cp ../coldkernel.config .config
 }
 
@@ -90,51 +90,51 @@ function build_kernel () {
 #  ------------------------------
 
 case "$1" in
-                -v)
-                import_keys &&
-		remove_wrkdir &&
-		get_kernel &&
-                get_patches &&
-                unpack_kernel &&
-                verify_kernel &&
-                verify_patches &&
-                extract_kernel &&
-                patch_kernel &&
-                build_kernel
-                exit 0;;
+    -v)
+        import_keys &&
+	    remove_wrkdir &&
+	    get_kernel &&
+            get_patches &&
+            unpack_kernel &&
+            verify_kernel &&
+            verify_patches &&
+            extract_kernel &&
+            patch_kernel &&
+            build_kernel
+	exit 0;;
 
-		*)
-		start_spinner "Importing signing keys..."
-		import_keys > /dev/null 2>&1 &&
-		sleep 1
-		stop_spinner $?
-		start_spinner "Removing previous working directory (if one exists)..."
-		remove_wrkdir > /dev/null 2>&1 &&
-		sleep 1
-		stop_spinner $?
-		start_spinner "Fetching kernel sources and signatures..."
-		get_kernel > /dev/null 2>&1 &&
-		stop_spinner $?
-		start_spinner "Fetching grsecurity patch and signatures..."
-		get_patches > /dev/null 2>&1 &&
-		stop_spinner $?
-		start_spinner "Unpacking Linux Kernel sources..."
-		unpack_kernel > /dev/null 2>&1 &&
-		stop_spinner $?
-		start_spinner "Verifying the Linux Kernel sources..."
-		verify_kernel > /dev/null 2>&1 &&
-		stop_spinner $?
-		start_spinner "Verifying Kernel patches..."
-		verify_patches > /dev/null 2>&1 &&
-		stop_spinner $?
-		start_spinner "Extracting Linux Kernel sources..."
-		extract_kernel > /dev/null 2>&1 &&
-		stop_spinner $?
-		start_spinner "Applying grsecurity patch, and moving coldkernel.config into place..."
-		patch_kernel > /dev/null 2>&1 &&
-		stop_spinner $?
-		start_spinner "Building coldkernel..."
-		build_kernel > /dev/null 2>&1
-		stop_spinner $?
-		exit 0;;
+     *)
+	 start_spinner "Importing signing keys..."
+	 import_keys > /dev/null 2>&1 &&
+	 sleep 1
+	 stop_spinner $?
+	 start_spinner "Removing previous working directory (if one exists)..."
+	 remove_wrkdir > /dev/null 2>&1 &&
+	 sleep 1
+	 stop_spinner $?
+	 start_spinner "Fetching kernel sources and signatures..."
+	 get_kernel > /dev/null 2>&1 &&
+	 stop_spinner $?
+	 start_spinner "Fetching grsecurity patch and signatures..."
+	 get_patches > /dev/null 2>&1 &&
+	 stop_spinner $?
+	 start_spinner "Unpacking Linux Kernel sources..."
+	 unpack_kernel > /dev/null 2>&1 &&
+	 stop_spinner $?
+	 start_spinner "Verifying the Linux Kernel sources..."
+	 verify_kernel > /dev/null 2>&1 &&
+	 stop_spinner $?
+	 start_spinner "Verifying Kernel patches..."
+	 verify_patches > /dev/null 2>&1 &&
+	 stop_spinner $?
+	 start_spinner "Extracting Linux Kernel sources..."
+	 extract_kernel > /dev/null 2>&1 &&
+	 stop_spinner $?
+	 start_spinner "Applying grsecurity patch, and moving coldkernel.config into place..."
+	 patch_kernel > /dev/null 2>&1 &&
+	 stop_spinner $?
+	 start_spinner "Building coldkernel..."
+	 build_kernel > /dev/null 2>&1
+	 stop_spinner $?
+	 exit 0;;
 esac
