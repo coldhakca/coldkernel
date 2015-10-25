de9d41c0205a2ec4f740f515b94a046e06080e28
Colin Childs <colin@torproject.org>
2015-10-24 21:54:07 -0500

    Reordering header

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Description = coldkernel build script
 # Script version = 0.4a
+# Code name = Egotistical Hamster
 # Kernel version = 4.2.4-grsec-coldkernel
 # Authors = coldhak (C. // J. // R. // T.)
-# Code name = Egotistical Hamster
 
 source "$(pwd)/spinner.sh"
 
