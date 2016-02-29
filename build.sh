3614e6193c221e65eeb75ed28b174d595be9a6f8
Colin Childs <colin@torproject.org>
2016-02-28 23:45:34 -0600

    Bumping kernel to 4.4.3

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 # Description = coldkernel build script
 # Script version = 0.8a
 # Code name = Furious Ferret
-# Kernel version = 4.4.2-coldkernel-grsec
+# Kernel version = 4.4.3-coldkernel-grsec
 # Authors = coldhak (C. // J. // R. // T.)
 
 set -e
@@ -12,7 +12,7 @@ source "$(pwd)/spinner.sh"
 
 SOURCE=https://www.kernel.org/pub/linux/kernel/v4.x
 KERNEL=linux
-VERSION=4.4.2
+VERSION=4.4.3
 GRSECVERSION=3.1
 NUM_CPUS=`grep -c ^processor /proc/cpuinfo`
 
